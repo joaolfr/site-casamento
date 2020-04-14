db9c2de62b34f6d95e3ffbacf4d745fa70ce7f0a
João Luiz <mac@MacBook-Air-de-mac.local>
2020-04-14 01:58:12 -0300

    changing webpack

--- src/pages/home/cards/index.js
+++ src/pages/home/cards/index.js
@@ -1,112 +1,109 @@
-import React, { Component } from 'react'
-import Grid from '@material-ui/core/Grid'
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
 
-import ChurchIcon from '../../../static/icons/IGREJA.png'
-import CameraIcon from '../../../static/icons/CAMERA.png'
-import Reception from '../../../static/icons/RECEPCAO.png'
-import Suit from '../../../static/icons/TRAJE.png'
-import './styles.css'
+import ChurchIcon from "../../../assets/icons/IGREJA.png";
+import CameraIcon from "../../../assets/icons/CAMERA.png";
+import Reception from "../../../assets/icons/RECEPCAO.png";
+import Suit from "../../../assets/icons/TRAJE.png";
+import "./styles.css";
 
 export default class cards extends Component {
   render() {
-    return(
-        <div className="div_cards">
-          <p id="dia_sim">
-            O dia do sim!
-          </p>
-          <p>
-          Esperamos que esse dia seja tão maravilhoso quanto a 
-          gente sonhou e as coisas vão acontecer da seguinte forma:
-          </p>
-          <Grid container id="container_cards">
-            <Grid item xs={12} md={6} id="item_cards">
-              <div id="card_1">
-                <div id="icon_div">
-                  <img  src={ChurchIcon} alt="Church"/>
-                </div>
-                <div id="info">
-                  <span id="cerimony">Cerimônia</span>
-                  <span id="time">às 16:00</span>
-                  <span id="place">Capela Santa Luzia</span>
-                  <span id="adress">R. Antonio Rocha, 555</span>
-                  
-                  <div id="btns_info">
-                    <button>
-                      <a href="https://github.com" id="waze_link">
-                        Waze
-                      </a>
-                    </button>
-                    <button>
-                      <a href="https://github.com" id="maps_link">
-                        Google maps
-                      </a>
-                    </button>
-                  </div>
-
-                </div>
+    return (
+      <div className="div_cards">
+        <p id="dia_sim">O dia do sim!</p>
+        <p>
+          Esperamos que esse dia seja tão maravilhoso quanto a gente sonhou e as
+          coisas vão acontecer da seguinte forma:
+        </p>
+        <Grid container id="container_cards">
+          <Grid item xs={12} md={6} id="item_cards">
+            <div id="card_1">
+              <div id="icon_div">
+                <img src={ChurchIcon} alt="Church" />
               </div>
-              <div id="card_1">
-                <div id="icon_div">
-                  <img  src={Reception} alt="Church"/>
-                </div>
-                <div id="info">
-                  <span id="cerimony">Recepção</span>
-                  <span id="time">após a cerimônia</span>
-                  <span id="place">Espaço Hibisco</span>
-                  <span id="adress">R. Olímpio Leite, 620</span>
-                  
-                  <div id="btns_info">
-                    <button>
-                      <a href="https://github.com" id="waze_link">
-                        Waze
-                      </a>
-                    </button>
-                    <button>
-                      <a href="https://github.com" id="maps_link">
-                        Google maps
-                      </a>
-                    </button>
-                  </div>
+              <div id="info">
+                <span id="cerimony">Cerimônia</span>
+                <span id="time">às 16:00</span>
+                <span id="place">Capela Santa Luzia</span>
+                <span id="adress">R. Antonio Rocha, 555</span>
 
-                </div>
-              </div>
-            </Grid>
-            <Grid item xs={12} md={6} id="item_cards">
-              
-              <div id="card_2">
-                <div id="icon_div">
-                  <img src={Suit} alt="Suit"/>
-                </div>
-
-                <div id="info_suit">
-                  <span id="suit">O traje é esporte fino</span>
-                  <span id="suit_tips">
-                  Estamos ao ar livre e no Ceará, então nada melhor 
-                  do que uma roupa leve e confortável, lá contamos com 
-                  grama também e talvez seja legal a mulherada 
-                  apostar no salto grosso.
-                  </span>
-
-                </div>
-              </div>
-
-              <div id="card_2">
-                <div id="uber">
-
-                  <span> Se divirta com responsabilidade</span>
-                  <span> O Uber da igreja para o buffet pode variar de R$15,00 a R$21,00</span>
+                <div id="btns_info">
                   <button>
-                    <a href="https://github.io">Pedir Uber</a>
+                    <a href="https://github.com" id="waze_link">
+                      Waze
+                    </a>
                   </button>
-                  <span>Basta clicar no botão para pedir o seu Uber de onde você estiver para a recepção</span>
-
+                  <button>
+                    <a href="https://github.com" id="maps_link">
+                      Google maps
+                    </a>
+                  </button>
                 </div>
               </div>
-              <div id="card_2"></div>
+            </div>
+            <div id="card_1">
+              <div id="icon_div">
+                <img src={Reception} alt="Church" />
+              </div>
+              <div id="info">
+                <span id="cerimony">Recepção</span>
+                <span id="time">após a cerimônia</span>
+                <span id="place">Espaço Hibisco</span>
+                <span id="adress">R. Olímpio Leite, 620</span>
 
-            </Grid>
+                <div id="btns_info">
+                  <button>
+                    <a href="https://github.com" id="waze_link">
+                      Waze
+                    </a>
+                  </button>
+                  <button>
+                    <a href="https://github.com" id="maps_link">
+                      Google maps
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
           </Grid>
-        </div>
+          <Grid item xs={12} md={6} id="item_cards">
+            <div id="card_2">
+              <div id="icon_div">
+                <img src={Suit} alt="Suit" />
+              </div>
+
+              <div id="info_suit">
+                <span id="suit">O traje é esporte fino</span>
+                <span id="suit_tips">
+                  Estamos ao ar livre e no Ceará, então nada melhor do que uma
+                  roupa leve e confortável, lá contamos com grama também e
+                  talvez seja legal a mulherada apostar no salto grosso.
+                </span>
+              </div>
+            </div>
+
+            <div id="card_2">
+              <div id="uber">
+                <span> Se divirta com responsabilidade</span>
+                <span>
+                  {" "}
+                  O Uber da igreja para o buffet pode variar de R$15,00 a
+                  R$21,00
+                </span>
+                <button>
+                  <a href="https://github.io">Pedir Uber</a>
+                </button>
+                <span>
+                  Basta clicar no botão para pedir o seu Uber de onde você
+                  estiver para a recepção
+                </span>
+              </div>
+            </div>
+            <div id="card_2"></div>
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
--- src/pages/home/header/index.js
+++ src/pages/home/header/index.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './styles.css'
-import Sub from '../../../static/images/plano.png';
-
+import "./styles.css";
+import Sub from "../../../assets/images/plano.png";
 
 export default class header extends Component {
   render() {
-    return(
-        <img id="subtracao" src={Sub} alt="subtracao" />
-    );
+    return <img id="subtracao" src={Sub} alt="subtracao" />;
   }
 }
