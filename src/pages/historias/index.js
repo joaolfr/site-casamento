ab66cebbd512efdd5b90b4ce387f21cd95ea0555
João Luiz <mac@MacBook-Air-de-mac.local>
2020-04-18 01:12:41 -0300

    finishing my text

--- src/pages/historias/index.js
+++ src/pages/historias/index.js
@@ -156,111 +156,96 @@ const Historias = () => {
             </div>
             <div className="version_text text_1">
               <p>
-                Passei 2 meses paquerando esse menino, mas ele é tão perspicaz
-                que não percebeu.
+                Percebi sim, mas era a filha do chefe né, me fiz de doido no
+                começo
               </p>
             </div>
             <span className="text_title">Começo de tudo</span>
             <div className="version_text text_2">
               <p>
-                Eu olhava pra ele assim que ele apareceu lá na HR e as pessoas
-                já sabiam que eu queria “dar uns pega” nele (até porque eu tinha
-                o discurso de que eu não queria casar, e aqui estamos). Antes
-                que eu pudesse perceber a gente tava tomando uns cafés um pouco
-                demorados enquanto conversávamos pra nos conhecer melhor.
-              </p>
-              <p>
-                Ele começou a me seguir no Instagram, e foi questão de tempo até
-                ele soltar aquele famoso “me dá teu Wpp aqui é muito ruim de
-                conversar”.
+                Minha situação era complicada, afinal dar em cima da filha do
+                chefe não é uma coisa muito aconselhável pra saúde. Mas fui
+                seguindo o fluxo, fomos nos conhecendo melhor e cada vez criado
+                mais afinidade, trocando mensagens pelo Instagram e não perdi
+                muito tempo, dei um jeito de arrumar o número dela :)
               </p>
             </div>
             <span className="text_title">A primeira cantada</span>
 
             <div className="version_text text_3">
               <p>
-                Era nítido que os dois tinham interesse, e ele provavelmente
-                esperou que eu desse em cima, e quando percebeu que não ia
-                acontecer ficou dando deixas, pra ver se eu me aproveitava da
-                situação.
+                Por mais que eu quisesse muito, tinha que ter muito cuidado,
+                afinal meu sogro tinha acabado de “perder” uma filha ( a Cinthya
+                tinha acabado de se casar), e chegar um zé doidin e roubar a
+                outra filha não era lá uma coisa muito interessante
               </p>
               <p>
-                Tal dia João me mandou uma foto (ainda pelo insta) sobre uma
-                apresentação que ele tava montando, já tinha dado pra perceber
-                que ele precisava de uma designer na vida dele, isso era tarde
-                da noite e tivemos uma conversa rápida que resultou em ele falar
-                que eu era má influência, eu confirmar. No outro dia enquanto eu
-                trabalhava depois do almoço ele senta no sofá da recepção olha
-                pra minha cara e pergunta na maior “quando tu vai me levar pro
-                mal caminho?”. MISERICÓRDIA, eu fiquei vermelha de vergonha e
-                desconversei.
+                Mas ambos já estavam muito a fim e era só questão de tempo até
+                rolar algo, certo dia ela me deu a deixa, falei que ela estava
+                me levando pro mal caminho e ela disse “você nem imagina!”. Ai
+                já viu, só esperei o momento certo pra perguntar quando ela iria
+                me levar pro mal caminho de verdade.
               </p>
             </div>
             <span className="text_title">O Primeiro Beijo</span>
 
             <div className="version_text text_4">
               <p>
-                Sem suspense, sim foi na empresa. No mesmo dia da pergunta do
-                mal caminho ele me chamou na copa e me fez a mesma pergunta, mas
-                bem mais de perto. Não deu outra né…
+                Chegou o dia de eu perguntar quando ela iria realmente me levar
+                pro mal caminho, quando ficamos a sós na cozinha tive a
+                oportunidade de olhar no olho dela e perguntar quando seria,
+                bem, não preciso nem falar o que aconteceu…
               </p>
             </div>
             <span className="text_title">O Primeiro Encontro</span>
 
             <div className="version_text text_5">
-              <p>Eu não vou nem comentar que fui eu quem tive que chamar…</p>
               <p>
-                Fomos a praia, lá a gente conversou sobre um monte de coisa,
-                descobriu algumas afinidades e eu apresentei a musica, “oi nego”
-                pra ele.
+                Fomos nos aproximando cada vez mais, mesmo ela passando 1 mês
+                longe mantivemos o contato e fomos nos conhecendo melhor mesmo
+                que a distância. Antes dela me ABANDONAR e viajar ela me chamou
+                pra sair e eu aceitei claro né.
               </p>
               <p>
-                A pérola mesmo foi porque eu ia viajar e passar um mês na casa
-                da minha irmã, eu acabei comentando com ele “ei, quero que tu
-                fique com saudade de mim!” e ele prontamente respondeu “por
-                que?” eu já havia comentado que passaria um mês em SP, e repeti
-                o motivo e ele respondeu “ah tá!” Isso gerou a primeira DR (que
-                foi enorme).
+                Foi um dia maravilhoso na praia, voltamos pra casa, pedimos
+                pizza e aproveitamos até o ultimo minuto, ela queria que eu
+                sentisse saudade dela quando viajasse, fui inventar de perguntar
+                “por que?”, me lasquei, até hoje ela lembra desse dia.
               </p>
             </div>
             <span className="text_title">O Pedido de Namoro</span>
 
             <div className="version_text text_6">
               <p>
-                Se o João me ouvisse era bom, eu pedi 92 mil vezes que ele não
-                me pedisse em namoro entre o aniversário dele e o meu (8 dias de
-                diferença) porque ia ficar muito complicado de comemorar, e um
-                ano depois ele percebeu isso.
-              </p>
-              <p>
-                Tirando isso, vou nem comentar que não teve como eu dizer não,
-                que foi uma surpresa linda e que foi o melhor presente de
-                aniversário.
+                Ela tentou falar pra eu nõo pedir ela em namoro em uma data
+                próxima a dos nossos aniversários ( ela faz aniversario poucos
+                dias depois do meu ) mas eu não podia perder a oportunidade
+                perfeita que era o dia da festa do aniversário dela. Tive muitos
+                cúmplices, até minha sogrinha querida participou da surpresa, e
+                ainda bem que deu tudo certo e ela aceitou.
               </p>
             </div>
             <span className="text_title">Com pouco menos...</span>
 
             <div className="version_text text_7">
               <p>
-                Com pouquíssimo tempo de namoro a gente já tinha escolhido o
-                nome da nossa filha e do nosso próximo cachorro, a gente já
-                dividia segredos e ficava imaginando como seria maravilhoso uma
-                vida a dois.
+                Nossa relação evoluiu muito rápido, mesmo com jeitos tão
+                diferentes a gente se completava de um jeito muito natural, e
+                mesmo com pouco tempo de relacionamento já tínhamos muitos
+                planos, decidimos até o nomo do nosso futuro cachorro o TUMI.
               </p>
             </div>
             <span className="text_title">Começamos a morar juntos</span>
 
             <div className="version_text text_8">
               <p>
-                Eu sempre falei que antes de casar faria um test drive pra ver
-                se eu aguentaria acordar e dormir com aquela pessoa, se era
-                aquilo mesmo que eu queria, e antes de continuar, sim, eu tenho
-                certeza que é isso que eu quero pro resto da vida.
-              </p>
-              <p>
-                E com alguns meses morando junto, e odiando esse rotulo, sem
-                previsão de casamento. João Luiz me pediu em casamento. E claro
-                que eu sonho com isso desde então.
+                Bem, comecei a dormir alguns dias com ela, e era maravilhoso
+                unir o útil com o agradável, dormir com quem você gosta e chegar
+                em 5 min no trabalho, não deu outra, com pouco tempo já estava
+                morando lá e ja começava a me sentir parte da família.
+                Percebemos então que tínhamos que oficializar de verdade o nosso
+                relacionamento, então pedi malove em casamento e começamos a
+                planejar cada vez mais o nosso futuro juntos.
               </p>
             </div>
           </div>
